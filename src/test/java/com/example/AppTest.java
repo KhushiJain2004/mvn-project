73d0e63fdde9a36f4b84cbdc4278934bd6e6cb1e
Khushi Jain <129265117+KhushiJain2004@users.noreply.github.com>
2025-05-18 01:50:46 +0530

    initial file

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void testGreet(){
+        assertEquals("DevOps", App.greet("DevOps"));
+    }
+}
